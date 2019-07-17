00c0ef66daf483be29fb509da7a3eeda5e065540
Juan Pablo Barrera Ospina <juan_barrerao@satrack.com>
2019-07-17 13:27:58 -0500

    creation project demo builder pattern structural

---
+++ BuilderEstructural/BuilderEstructural/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderEstructural
+{
+    class Director
+    {
+        // Builder uses a complex series of steps
+        public void Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+}
